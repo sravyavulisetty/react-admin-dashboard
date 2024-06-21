4e3ab57c4b63e251a1a44cc4ac84e34485d4d5b0
sravyavulisetty <sravyavulisetty269@gmail.com>
2024-06-21 10:14:35 -0400

    dashboard path fixed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import { ColorModeContext, useMode } from "./theme";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Team from "./pages/team";
-import Invoices from "./pages/invoices";
-import Bar from "./pages/bar";
-import Contacts from "./pages/contacts";
-import Form from "./pages/form";
-import Line from "./pages/line";
-import Pie from "./pages/pie";
-import FAQ from "./pages/faq";
-import Geography from "./pages/geography";
-import Calendar from "./pages/calendar";
+import Dashboard from '../src/pages/Dashboard/index'
+import Team from "../src/pages/team/index";
+import Invoices from "../src/pages/invoices/index";
+import Bar from "../src/pages/bar/index";
+import Contacts from "../src/pages/contacts/index";
+import Form from "../src/pages/form/index";
+import Line from "../src/pages/line/index";
+import Pie from "../src/pages/pie/index";
+import FAQ from "../src/pages/faq/index";
+import Geography from "../src/pages/geography/index";
+import Calendar from "../src/pages/calendar/index";
 
 const App = () => {
   const [theme, colorMode] = useMode();
